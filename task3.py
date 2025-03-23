a49b8da0dedd0258fcbd5f7ada0dc8d03e71dd4a
remake_get <getmaximuminlife@gmail.com>
2025-03-23 13:17:06 +0500

    added average grades method

--- task3.py
+++ task3.py
@@ -7,6 +7,22 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def __str__(self):
+        result = f'Имя: {self.name}\nФамилия: {self.surname}\n'
+        avg_grd = self.average_grade()
+        result += f'Средняя оценка за домашние задания: {avg_grd}\n'
+        result += f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
+        result += f'Завершенные курсы: {", ".join(self.finished_courses)}\n'    
+        return result       
+
+    def average_grade(self):
+        total_grades = 0
+        total_courses = 0
+        for grades in self.grades.values():
+            total_grades += sum(grades)
+            total_courses += len(grades)
+        return total_grades / total_courses if total_courses > 0 else 0
+
     def rate_lecturer(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
             if course in lecturer.grades:
@@ -28,15 +44,25 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+
     def __str__(self):
-        result = f'Лектор: {self.name} {self.surname}\n'
-        for course, grades in self.grades.items():
-            result += f'Курс: {course}, Оценки: {grades}\n'
+        result = f'Имя: {self.name}\nФамилия: {self.surname}\n'
+        avg_grd = self.average_grade()
+        result += f'Средняя оценка за лекции: {avg_grd}\n'
         return result    
-        
+    
+    def average_grade(self):
+        total_grades = 0
+        total_courses = 0
+        for grades in self.grades.values():
+            total_grades += sum(grades)
+            total_courses += len(grades)
+        return total_grades / total_courses if total_courses > 0 else 0
+    
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
+
     def __str__(self):
         result =  f'Имя: {self.name}\nФамилия: {self.surname}\n'
         return result 
@@ -50,20 +76,22 @@ class Reviewer(Mentor):
         else:
             return 'Ошибка'    
 
-best_student1 = Student('Ruoy', 'Eman', 'your_gender')
-best_student2 = Student('Vanya', 'Ivanov', 'your_gender')
-best_student1.courses_in_progress += ['Python']
-best_student2.courses_in_progress += ['Ruby']
- 
-cool_mentor = Reviewer('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student1, 'Python', 10)
-cool_mentor.rate_hw(best_student1, 'Python', 10)
-cool_mentor.rate_hw(best_student1, 'Python', 10)
- 
-print(best_student1.grades)
+some_student = Student('Ruoy', 'Eman', 'Male')
+some_student2 = Student('Vanya', 'Ivanov', 'Male')
+some_student.courses_in_progress += ['Python']
+some_student.courses_in_progress += ['Git']
+some_student.finished_courses += ['Введение в программирование']
 
+some_student2.courses_in_progress += ['Ruby']
+ 
+some_reviewer = Reviewer('Some', 'Buddy')
+some_reviewer.courses_attached += ['Python']
+some_reviewer.courses_attached += ['Git']
+ 
+some_reviewer.rate_hw(some_student, 'Python', 10)
+some_reviewer.rate_hw(some_student, 'Python', 4)
+some_reviewer.rate_hw(some_student, 'Python', 10)
+some_reviewer.rate_hw(some_student, 'Git', 5)
 
 best_lecturer1 = Lecturer('Johny', 'Ivanov')
 best_lecturer2 = Lecturer('Stan', 'Petrov')
@@ -71,11 +99,15 @@ best_lecturer1.courses_attached += ['Ruby']
 best_lecturer2.courses_attached += ['Python']
 
 
-best_student1.rate_lecturer(best_lecturer2,'Python', 9)
-best_student1.rate_lecturer(best_lecturer2,'Python', 9)
-best_student2.rate_lecturer(best_lecturer1,'Ruby', 10)
-best_student2.rate_lecturer(best_lecturer1,'Ruby', 9)
+some_student.rate_lecturer(best_lecturer2,'Python', 9)
+some_student.rate_lecturer(best_lecturer2,'Python', 5)
+some_student2.rate_lecturer(best_lecturer1,'Ruby', 10)
+some_student2.rate_lecturer(best_lecturer1,'Ruby', 3)
 
+print(some_reviewer)
 print(best_lecturer1)
 print(best_lecturer2)
-print(cool_mentor)
+print(some_student)
+
+print(some_student > some_student2)  # Сравнение студентов по средней оценке
+print(best_lecturer1 < best_lecturer2)  # Сравнение лекторов по средней оценке
